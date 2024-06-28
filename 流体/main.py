2713ab40a69ec82b0c6d7732eb87e3208356b72b
aFei-CQUT <2039787966@qq.com>
2024-06-28 20:31:58 +0800

    Add Compress Function
    添加压缩功能

--- 流体/main.py
+++ 流体/main.py
@@ -24,6 +24,33 @@ if __name__ == "__main__":
         r"./离心泵特性曲线拟合.py",
         r"./流体阻力曲线拟合.py",
     ]
+    
+
+    '''
+    拟合图结果压缩
+    '''
+    import zipfile
+    import os
+
+    # 定义要压缩的目录
+    directory_to_zip = r'./拟合图结果'
+
+    # 定义压缩文件的名称
+    zip_file_name = r'./拟合图结果.zip'
+
+    # 创建ZipFile对象
+    with zipfile.ZipFile(zip_file_name, 'w', zipfile.ZIP_DEFLATED) as zipf:
+
+        # 遍历目录
+        for root, dirs, files in os.walk(directory_to_zip):
+            for file in files:
+
+                # 创建相对文件路径并将其写入zip文件
+                file_path = os.path.join(root, file)
+                arcname = os.path.relpath(file_path, directory_to_zip)
+                zipf.write(file_path, arcname)
+
+    print(f'压缩完成，文件保存为: {zip_file_name}')
 
     for script in scripts:
         run_script(script)
